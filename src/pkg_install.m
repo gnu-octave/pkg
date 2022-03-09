24f620766766729203ed91b314816cd94651bc62
Kai T. Ohlhus <k.ohlhus@gmail.com>
2022-03-09 18:39:17 +0900

    src/pkg_install.m: remove outsourced legacy code

--- src/pkg_install.m
+++ src/pkg_install.m
@@ -110,7 +110,7 @@ function pkg_install (varargin)
   unwind_protect
 
     if (params.flags.("-forge"))
-      [urls, local_files] = cellfun ("get_forge_download", files,
+      [urls, local_files] = cellfun ("legacy_forge_download", files,
                                      "uniformoutput", false);
       [files, succ] = cellfun ("urlwrite", urls, local_files,
                                "uniformoutput", false);
@@ -988,80 +988,5 @@ function generate_lookfor_cache (desc)
   for i = 1 : length (dirs)
     doc_cache_create (fullfile (dirs{i}, "doc-cache"), dirs{i});
   endfor
-
-endfunction
-
-
-function [url, local_file] = get_forge_download (name)
-  [ver, url] = get_forge_pkg (name);
-  local_file = tempname (tempdir (), [name "-" ver "-"]);
-  local_file = [local_file ".tar.gz"];
-endfunction
-
-
-function [ver, url] = get_forge_pkg (name)
-
-## Try to discover the current version of an Octave Forge package from the web,
-## using a working internet connection and the urlread function.
-## If two output arguments are requested, also return an address from which
-## to download the file.
-
-  ## Verify that name is valid.
-  if (! (ischar (name) && rows (name) == 1 && ndims (name) == 2))
-    error ("get_forge_pkg: package NAME must be a string");
-  elseif (! all (isalnum (name) | name == "-" | name == "." | name == "_"))
-    error ("get_forge_pkg: invalid package NAME: %s", name);
-  endif
-
-  name = tolower (name);
-
-  ## Try to download package's index page.
-  [html, succ] = urlread (sprintf ("https://packages.octave.org/%s/index.html",
-                                   name));
-  if (succ)
-    ## Remove blanks for simpler matching.
-    html(isspace (html)) = [];
-    ## Good.  Let's grep for the version.
-    pat = "<tdclass=""package_table"">PackageVersion:</td><td>([\\d.]*)</td>";
-    t = regexp (html, pat, "tokens");
-    if (isempty (t) || isempty (t{1}))
-      error ("get_forge_pkg: could not read version number from package's page");
-    else
-      ver = t{1}{1};
-      if (nargout > 1)
-        ## Build download string.
-        pkg_file = sprintf ("%s-%s.tar.gz", name, ver);
-        url = ["https://packages.octave.org/download/" pkg_file];
-        ## Verify that the package string exists on the page.
-        if (isempty (strfind (html, pkg_file)))
-          warning ("get_forge_pkg: download URL not verified");
-        endif
-      endif
-    endif
-  else
-    ## Try get the list of all packages.
-    [html, succ] = urlread ("https://packages.octave.org/list_packages.php");
-    if (! succ)
-      error ("get_forge_pkg: could not read URL, please verify internet connection");
-    endif
-    t = strsplit (html);
-    if (any (strcmp (t, name)))
-      error ("get_forge_pkg: package NAME exists, but index page not available");
-    endif
-    ## Try a simplistic method to determine similar names.
-    function d = fdist (x)
-      len1 = length (name);
-      len2 = length (x);
-      if (len1 <= len2)
-        d = sum (abs (name(1:len1) - x(1:len1))) + sum (x(len1+1:end));
-      else
-        d = sum (abs (name(1:len2) - x(1:len2))) + sum (name(len2+1:end));
-      endif
-    endfunction
-    dist = cellfun ("fdist", t);
-    [~, i] = min (dist);
-    error ("get_forge_pkg: package not found: ""%s"".  Maybe you meant ""%s?""",
-           name, t{i});
-  endif
 
 endfunction
