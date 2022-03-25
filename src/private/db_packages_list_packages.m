722e0a35ab8fd12aeb1ba423ce1a5e2ad87680a7
Kai T. Ohlhus <k.ohlhus@gmail.com>
2022-03-25 17:35:58 +0900

    Cache Octave packages index for offline usage

--- src/private/db_packages_list_packages.m
+++ src/private/db_packages_list_packages.m
@@ -82,10 +82,35 @@ endfunction
 
 
 function __pkg__ = package_index_resolve ()
-  data = urlread ("https://gnu-octave.github.io/packages/packages/")(6:end);
+
+  cache_dir = [pkg_config()].cache_dir;
+  cache_file = fullfile (cache_dir, "octave_package_index.mat");
+
+  try
+    data = urlread ("https://gnu-octave.github.io/packages/packages/")(6:end);
+  catch
+    if (exist (cache_file, "file") == 2)
+      load (cache_file, "__pkg__", "timestamp");
+      ## Warn if package information is older than a day.
+      age_in_days = (time () - timestamp) / 60;
+      if (age_in_days > 1)
+        warning (["pkg>db_packages_list_packages: could not retrieve ", ...
+          "package information from the internet.\n  The cached package ", ...
+          "information is %.0f day(s) old.\n"], age_in_days);
+      endif
+      return
+    else
+      error (["pkg>db_packages_list_packages: checksums corrupt.\nPlease ", ...
+        "report a bug at <https://github.com/gnu-octave/packages/issues>."]);
+    endif
+  end_try_catch
   data = strrep (data, "&gt;",  ">");
   data = strrep (data, "&lt;",  "<");
   data = strrep (data, "&amp;", "&");
   data = strrep (data, "&#39;", "'");
   eval (data);
+  if (exist (cache_dir, "dir") == 7)
+    timestamp = time ();
+    save (cache_file, "__pkg__", "timestamp");
+  endif
 endfunction
